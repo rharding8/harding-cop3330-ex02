7ebf2d170aa9a658ca7f28294a6010e17067053d
Ryan Harding <darkstar100x@MacBook-Pro.local>
2021-06-06 12:02:05 -0400

    Initial commit

---
+++ src/main/java/ex02/base/StringCount.java
@@ -0,0 +1,22 @@
+package ex02.base;
+
+import java.util.Scanner;
+
+public class StringCount {
+  Scanner input = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    StringCount count = new StringCount();
+    String inputString = count.inString();
+    System.out.println(count.outString(inputString));
+  }
+
+  public String inString() {
+    System.out.print("Enter an input string: ");
+    return input.nextLine();
+  }
+
+  public String outString(String in) {
+    return String.format("%s has %d characters.", in, in.length());
+  }
+}
